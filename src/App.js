605b8e239b7aa454d9113d6c667279a6033393e4
Marylo0ove <94191727+Marylo0ove@users.noreply.github.com>
2022-09-14 17:23:02 +0200

    Mise en place
    Mise en place de l'environnement de travail
    npx create-react-app
    git init
    npm i -s react-router-dom sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter,Routes, Route} from "react-router-dom";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Error404 from './pages/Error404';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+        <Route path="/Home" element={<Home/>} />
+        <Route path="*" element={<Error404/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
