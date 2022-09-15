3b6c7590d863ed027906e5b4f8e88ef63224f63c
Marylo0ove <94191727+Marylo0ove@users.noreply.github.com>
2022-09-15 11:21:25 +0200

    Boostrap + SignIn
    npm install bootstrap@5.2.1
    npm install react-bootstrap boostrap
    
    Première ébauche de la page SignIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
 const SignIn = () => {
     return (
-        <div>
-            Sign In
-        </div>
+        <Container className='text-center'>
+            <Form>
+              <img src='./assets/icon-left-font.png' alt='logo groupomania' /> 
+              <h1>Connexion</h1> 
+            </Form>
+            <Form.Group controlId='sign-in-email-adress'>
+                <Form.Control type='email' size='lg' placeholder='Adresse mail' autoComplete='username'/>
+            </Form.Group>
+            <Form.Group controlId='sign-in-password'>
+                <Form.Control type='password' size='lg' placeholder='Mot de passe' autoComplete='current-password'/>
+            </Form.Group>
+            <Button variant='primary' size='lg'>Connexion</Button>
+        </Container>
     );
 };
 
